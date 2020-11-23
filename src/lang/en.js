de1e6c8eebf55802ad99d1001f0b37662dac4f5c
RK <guiltyparties@protonmail.com>
2020-11-22 19:05:22 -0500

    Rewrote the Hive blurb

--- src/lang/en.js
+++ src/lang/en.js
@@ -15,7 +15,7 @@ export default {
   },
   about: {
     title: 'Developed for Web',
-    text: `Hive is an innovative and forward-looking decentralized blockchain and ecosystem built on the Delegated Proof of Stake (DPoS) protocol. Since its inception, Hive has steadily grown as an ecosystem. Myriad dapps, APIs and front-ends contribute to a general and straightforward accessibility of data, transactions and records, so that this existing diversity and utility ensure that the ecosystem is welcoming to content creators, consumers, investors and builders. Hive is developed to store vast amounts of content and to make it available for time-based monetization. The performance of the blockchain is designed to scale with widespread adoption of the currency and platforms in mind. By combining the lightning fast processing times and fee-less transactions, Hive is positioned to become one of the leading blockchains used by people around the world.`,
+    text: `Hive is an innovative and forward-looking decentralized blockchain and ecosystem, designed to scale with widespread adoption of the currency and platforms in mind. By combining the lightning-fast processing times and fee-less transactions, Hive is positioned to become one of the leading Web 3.0 blockchains used by people around the world.`,
     featureFast: 'Fast',
     featureFastText: 'Fee-less transactions take 3 seconds.',
     featureScalable: 'Scalable',

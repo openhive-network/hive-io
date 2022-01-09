a784597d7e4dea90d7084bd15706fbf803c94a9c
Mahdi Yari <m.yary15@gmail.com>
2022-01-10 02:45:55 +0330

    top menu whyhive button

--- src/helpers/navigation.ts
+++ src/helpers/navigation.ts
@@ -23,6 +23,11 @@ export const NAVIGATION_HEADER = [
     name: 'Join',
     isButton: true,
   },
+  {
+    to: 'https://whyhive.co',
+    name: 'Why Hive?',
+    isButton: true,
+  },
 ]
 
 /**
@@ -111,7 +116,7 @@ export const NAVIGATION_FOOTER = [
       to: 'https://hive.io/whitepaper.pdf',
       name: 'Whitepaper',
     },
-        {
+    {
       to: 'https://hive.blue/docs/technical-vision.pdf',
       name: 'Technical Vision',
     },

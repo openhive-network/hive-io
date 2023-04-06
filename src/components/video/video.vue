f9840b5299c6d747296c96be3b50036ba4c6502e
Mahdi Yari <m.yary15@gmail.com>
2023-04-07 01:26:38 +0330

    video component

---
+++ src/components/video/video.vue
@@ -0,0 +1,21 @@
+<template>
+  <video class="video" controls>
+    <source src="~/assets/videos/marketing.mp4" type="video/mp4">
+  </video>
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss">
+.video {
+  width: auto;
+  height: auto;
+}
+</style>

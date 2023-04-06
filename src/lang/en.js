c292abed2750becad9b179d55d4779ea00fa0a7b
Mahdi Yari <m.yary15@gmail.com>
2023-04-06 18:04:24 +0330

    fast-text-title

--- src/lang/en.js
+++ src/lang/en.js
@@ -20,9 +20,14 @@ export default {
         'Hive is available at the top exchanges for secure transactions',
     },
     hbdTitle: 'Hive Backed Dollar',
-    hbdText: 'Enjoy safe investment on a decentralized stable coin powered by Hive blockchain. HBD is backed by HIVE and currently provides one of the highest interest rates in the crypto space.',
+    hbdText:
+      'Enjoy safe investment on a decentralized stable coin powered by Hive blockchain. HBD is backed by HIVE and currently provides one of the highest interest rates in the crypto space.',
     hbdAPR: '20% APR on HBD savings.',
-    hbdButton: 'Learn more'
+    hbdButton: 'Learn more',
+    fastTitle: 'Fast',
+    fastText:
+      'Transactions are processed and confirmed in 3 seconds. Hive is using OBI (one-block irreversibility) meaning that transactions become irreversible in less than 3 seconds. No need to wait for confirmations.',
+    fastButton: 'Learn more',
   },
   about: {
     title: 'Built for Web',
@@ -37,8 +42,7 @@ export default {
   },
   eco: {
     title: 'Hive Ecosystem',
-    text:
-      'Below is a selection of apps & communities leveraging Hive.',
+    text: 'Below is a selection of apps & communities leveraging Hive.',
     subTitle: 'Interested in statistics?',
     subText:
       'Thousands of people across numerous dApps are using Hive every day.',
@@ -50,16 +54,21 @@ export default {
   },
   hbd: {
     title: 'Hive Backed Dollar',
-    text: 'HBD is a native stablecoin on the Hive blockchain. HBD is built to hold $1USD value in the most decentralized ways. It benefits from the fee-less and fast transactions of the Hive blockchain.',
+    text:
+      'HBD is a native stablecoin on the Hive blockchain. HBD is built to hold $1USD value in the most decentralized ways. It benefits from the fee-less and fast transactions of the Hive blockchain.',
     howTitle: 'How does it maintain the $1USD peg?',
-    howText: 'There are two decentralized conversion operations designed to achieve this goal. HIVE to HBD conversion and HBD to HIVE conversion. These two operations keep the price at $1USD with a tight margin.',
+    howText:
+      'There are two decentralized conversion operations designed to achieve this goal. HIVE to HBD conversion and HBD to HIVE conversion. These two operations keep the price at $1USD with a tight margin.',
     hbdToHiveTitle: 'HBD to HIVE conversion',
-    hbdToHiveText: 'This operation converts set amounts of HBD to $1USD worth of HIVE with no extra fees. It takes 3.5 days for the conversion to finalize. The price in this conversion is the median price of the coming 3.5 days to prevent market manipulations.',
+    hbdToHiveText:
+      'This operation converts set amounts of HBD to $1USD worth of HIVE with no extra fees. It takes 3.5 days for the conversion to finalize. The price in this conversion is the median price of the coming 3.5 days to prevent market manipulations.',
     hiveToHbdTitle: 'HIVE to HBD conversion',
-    hiveToHbdText: 'This operation is a collateral-based conversion. After initialazing the conversion of the X amount of HIVE to HBD, the user gets Y amount of HBD immediately. The Y is equivilent of the value of X/2 of the HIVE. The cost for Y amount of HBD is calculated after 3.5 days and the remaining HIVE is returned to the user. The price is the median price of the 3.5 days. This conversion has a 5% fee which is burned in the process.',
+    hiveToHbdText:
+      'This operation is a collateral-based conversion. After initialazing the conversion of the X amount of HIVE to HBD, the user gets Y amount of HBD immediately. The Y is equivilent of the value of X/2 of the HIVE. The cost for Y amount of HBD is calculated after 3.5 days and the remaining HIVE is returned to the user. The price is the median price of the 3.5 days. This conversion has a 5% fee which is burned in the process.',
     supportedWallets: 'Supported wallets',
     interestTitle: 'Savings account APR',
-    interestText: 'Placing your HBD savings in a Savings Account on Hive will generate returns. Saving is an action that locks the HBD with a unlock time of only 3 days. The interest rate is voted by the witnesses and is variable. It is currently at 20% APR.',
+    interestText:
+      'Placing your HBD savings in a Savings Account on Hive will generate returns. Saving is an action that locks the HBD with a unlock time of only 3 days. The interest rate is voted by the witnesses and is variable. It is currently at 20% APR.',
     whereToBuyTitle: 'Where to buy',
     moreInfoTitle: 'Statistics, market cap, and more',
     moreInfoText: 'You can grab the latest figures at ',

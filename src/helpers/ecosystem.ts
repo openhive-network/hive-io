74b4d3c7a9099bd7fe5dd379a584cbac7a27affb
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-12-03 13:09:17 -0300

    skatehive tags

--- src/helpers/ecosystem.ts
+++ src/helpers/ecosystem.ts
@@ -155,7 +155,7 @@ export const ECOSYSTEM: IEcoItem[] = [
       'The web3 portal for skateboarding. A decentralized social media platform for skateboarders.',
     image: 'skatehive-logo.png',
     website: 'https://skatehive.app',
-    types: [EcoType.social],
+    types: [EcoType.social, EcoType.video, EcoType.sport],
   },
   // {
   //   id: 'stemsocial',

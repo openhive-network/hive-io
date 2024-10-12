f230fe73c39470d68786c6dbcc9af8e52110ae8c
RK <guiltyparties@protonmail.com>
2024-10-12 02:13:36 +0000

    Update ecosystem.ts

--- src/helpers/ecosystem.ts
+++ src/helpers/ecosystem.ts
@@ -225,7 +225,7 @@ export const ECOSYSTEM: IEcoItem[] = [
     name: 'LeoDex',
     description:
       'Trade HIVE to and from the top crypto assets in the world like BTC, ETH, DASH and more. LeoDex is a DEX Web app for cross-chain trading that prioritizes a seamless User Experience with a clean and intuitive Interface as a non-custodial decentralized exchange. Trading across blockchains on LeoDex is extremely easy and can be done using Maya Protocol and Hive Aggregation Technology.',
-    image: 'LeoDex.png',
+    image: 'LeoDex_Logo_no_Name.png',
     website: 'https://leodex.io/',
     types: [EcoType.defi],
     featured: false,

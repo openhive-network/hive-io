80c555fdc09e1120b787f2a806a5af7bcd7e1a12
Sagar <sag333ar@gmail.com>
2026-05-20 05:52:36 +0530

    removed hreplier & added hive-suite app

--- apps/web/src/lib/data/ecosystem.ts
+++ apps/web/src/lib/data/ecosystem.ts
@@ -546,11 +546,11 @@ export const ECOSYSTEM: IEcoItem[] = [
     types: [EcoType.game],
   },
   {
-    id: 'hreplier',
-    name: 'hReplier',
-    description: "Helps you manage Hive Blog's replies.",
-    image: 'hive-inbox.png',
-    website: 'https://hreplier.sagarkothari88.one',
+    id: 'hive-suite',
+    name: 'HiveSuite',
+    description: 'All in one - blogs, videos, shorts, polls, snaps & games',
+    image: 'hive-suite-logo.png',
+    website: 'https://hivesuite.app',
     types: [EcoType.social],
   },
   {
@@ -570,14 +570,14 @@ export const ECOSYSTEM: IEcoItem[] = [
     website: 'https://snapie.xyz/',
     types: [EcoType.social],
   },
-//  {
-//    id: 'hive-analytics',
-//   name: 'Hive Analytics',
-//    description: 'Real-time blockchain intelligence for the Hive ecosystem.',
-//    image: 'hiveanalytics.png',
-//    website: 'https://hiveanalytics.usehive.com/',
-//    types: [EcoType.tools],
-//  },
+  //  {
+  //    id: 'hive-analytics',
+  //   name: 'Hive Analytics',
+  //    description: 'Real-time blockchain intelligence for the Hive ecosystem.',
+  //    image: 'hiveanalytics.png',
+  //    website: 'https://hiveanalytics.usehive.com/',
+  //    types: [EcoType.tools],
+  //  },
 ].concat(BLOCKEXPLORERS as any)
 
 export const TYPE_COLORS = {
